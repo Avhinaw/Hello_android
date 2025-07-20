9e1fddd675d5f9fb1748a22be92be413357861ce
avhinaw <avhinaaw@gmail.com>
2025-07-20 22:52:05 +0530

    cors code added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,20 @@
 import express from 'express';
+import chatRoutes from './routes/chatRoute';
+import { connectDb } from './config/db';
+import cors from 'cors';
 require('dotenv').config();
 
 const PORT = process.env.PORT;
 const app = express();
+app.use(cors ({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+    credentials: true
+}));
+app.use(express.json());
+connectDb();
+app.use('/api', chatRoutes);
 
-app.get('/', (req, res) => {
-    res.send("hey there");
-});
 app.listen(PORT, () => {
-    console.log("started");
+    console.log(`Server is running on port ${PORT}`);
 });
