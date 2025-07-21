9d9e241411d29d6eda7bc5a2157521dc05129b45
avhinaw <avhinaaw@gmail.com>
2025-07-21 14:29:21 +0530

    added responsiveness

--- frontend/src/app/components/SettingModal.tsx
+++ frontend/src/app/components/SettingModal.tsx
@@ -6,8 +6,8 @@ export default function SettingModal() {
   const { showSettingsModal } = useModal();
   if (!showSettingsModal) return null;
   return (
-    <div className='backdrop-blur-lg bg-[rgba(85,83,63,0.3)] p-6 text-whitebg-transparent shadow-lg w-screen h-screen z-5 absolute rounded-xl transform transition-all duration-500 ease-in-out space-y-4 dark:text-white'>
-    <div className='backdrop-blur-lg bg-[rgba(85,83,63,0.3)] p-6 text-whitebg-transparent shadow-lg w-[25vw] h-[38vh] z-5 absolute bottom-70 left-130 rounded-xl transform transition-all duration-1000 ease-in-out space-y-4 dark:text-white'>
+    <div className='backdrop-blur-lg bg-[rgba(85,83,63,0.3)] p-6 text-whitebg-transparent shadow-lg w-screen h-screen z-55 absolute rounded-xl transform transition-all duration-500 ease-in-out space-y-4 dark:text-white'>
+    <div className='backdrop-blur-lg bg-[rgba(85,83,63,0.3)] p-6 text-whitebg-transparent shadow-lg lg:w-[25vw] lg:h-[38vh] z-5 absolute lg:bottom-70 lg:left-130 rounded-xl transform transition-all duration-1000 ease-in-out space-y-4 dark:text-white'>
         <h2 className='text-lg font-bold'>Settings</h2>
         <div>
         <h4 className='font-semibold'>Theme</h4>
