e4b34f5f8633c4fa896140e656d81d2162695912
avhinaw <avhinaaw@gmail.com>
2025-07-22 11:15:02 +0530

    added further responsive

--- frontend/src/app/components/ChatInterface.tsx
+++ frontend/src/app/components/ChatInterface.tsx
@@ -8,7 +8,7 @@ function ChatInterface() {
   const { messages } = useChat();
 
   return (
-    <div className="relative w-screen lg:w-[70vw] h-[87vh] flex flex-col dark:bg-gray-800">
+    <div className="relative w-screen lg:w-[70vw] h-[87vh] lg:h-[100vw] flex flex-col dark:bg-gray-800">
       
       {/* Scrollable messages */}
       <div className="flex-1 overflow-y-auto px-2 lg:px-4 pb-24 pt-8 flex flex-col gap-8">
@@ -24,7 +24,7 @@ function ChatInterface() {
       </div>
 
       {/* Mobile-only chat input */}
-      <div className="sticky bottom-0 left-0 w-full md:hidden z-50 bg-white dark:bg-gray-900 px-4 py-2 border-t border-gray-300 dark:border-gray-700">
+      <div className="sticky bottom-0 left-0 w-full md:hidden z-50 bg-gray-100 dark:bg-gray-900 px-4 py-2 border-t border-gray-300 dark:border-gray-700">
         <ChatInput />
       </div>
     </div>
